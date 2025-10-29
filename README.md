2f707fda8ebee7e7949042964b82b4cb36a67f98
leehyokyun <gyrbs22@gmail.com>
2025-10-29 18:05:47 +0900

    readme / entry point

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-spring batch boot
+## 1. 개요
+
+Spring Boot 환경에서 Spring Batch Framework를 실무적으로 활용하기 위한 방법론들을 학습하여 기록한다.
--- src/main/java/com/system/batch/KillBatchSystemApplication.java
+++ src/main/java/com/system/batch/KillBatchSystemApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KillBatchSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KillBatchSystemApplication.class, args);
+		/*
+		* 배치 작업의 성공/실패 상태를 exit code로 외부 시스템에 전달
+		* 실무에서 배치 모니터링과 제어에 필수적
+		* */
+		System.exit(SpringApplication.exit(SpringApplication.run(KillBatchSystemApplication.class, args)));
 	}
 
 }
