f2abbcbd3f244be63d93adb797eaef12f40375cb
leehyokyun <gyrbs22@gmail.com>
2025-11-13 19:41:53 +0900

    cli - enum

--- batch/jobParameters/src/main/java/com/system/batch/config/SystemTerminatorConfig.java
+++ batch/jobParameters/src/main/java/com/system/batch/config/SystemTerminatorConfig.java
@@ -1,5 +1,6 @@
 package com.system.batch.config;
 
+import com.system.batch.util.QuestDifficulty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -31,25 +32,50 @@ public class SystemTerminatorConfig {
                 .build();
     }
 
+//    @Bean
+//    @StepScope
+//    public Tasklet terminatorTasklet(
+//            //lombok value - 불변 클래스 생성
+//            //beas.factory - SpEL(Spring Expression Language(CLI)를 통해 프로퍼티/jobparameter 값 주입 시
+//            @Value("#{jobParameters['terminatorId']}") String terminatorId,
+//            @Value("#{jobParameters['targetCount']}") Integer targetCount
+//    ) {
+//        return (contribution, chunkContext) -> {
+//          log.info("Processing terminator tasklet");
+//          log.info("Terminator tasklet id : {}", terminatorId);
+//          log.info("Terminator tasklet targetCount : {}", targetCount);
+//
+//          for(int i = 0 ; i <= targetCount ; i++){
+//              log.info("target terminating System is Running : {}/{}", i, targetCount);
+//          }
+//
+//          log.info("Terminator tasklet finished");
+//          return RepeatStatus.FINISHED;
+//        };
+//    }
+
     @Bean
     @StepScope
     public Tasklet terminatorTasklet(
             //lombok value - 불변 클래스 생성
             //beas.factory - SpEL(Spring Expression Language(CLI)를 통해 프로퍼티/jobparameter 값 주입 시
-            @Value("#{jobParameters['terminatorId']}") String terminatorId,
-            @Value("#{jobParameters['targetCount']}") Integer targetCount
-    ) {
+            @Value("#{jobParameters['questDifficulty']}")QuestDifficulty questDifficulty
+            ) {
         return (contribution, chunkContext) -> {
-          log.info("Processing terminator tasklet");
-          log.info("Terminator tasklet id : {}", terminatorId);
-          log.info("Terminator tasklet targetCount : {}", targetCount);
+            log.info("Processing terminator tasklet");
+            log.info("Terminator tasklet quest difficulty : {}", questDifficulty);
 
-          for(int i = 0 ; i <= targetCount ; i++){
-              log.info("target terminating System is Running : {}/{}", i, targetCount);
-          }
+            log.info("reward is based on quest difficulty.");
+            int reward = switch (questDifficulty){
+                case EASY -> 1;
+                case NORMAL -> 2;
+                case HARD -> 3;
+                case EXTREME -> 4;
+            };
+            log.info("finially reward is : {}", reward);
 
-          log.info("Terminator tasklet finished");
-          return RepeatStatus.FINISHED;
+            log.info("Terminator tasklet finished");
+            return RepeatStatus.FINISHED;
         };
     }
 }
---
+++ batch/jobParameters/src/main/java/com/system/batch/util/QuestDifficulty.java
@@ -0,0 +1,8 @@
+package com.system.batch.util;
+
+public enum QuestDifficulty {
+    EASY,
+    NORMAL,
+    HARD,
+    EXTREME
+}
