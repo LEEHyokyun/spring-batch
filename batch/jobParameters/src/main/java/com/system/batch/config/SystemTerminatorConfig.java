60eb2d6868e5af28d5132b444c51a835beef8029
leehyokyun <gyrbs22@gmail.com>
2025-11-14 19:14:11 +0900

    jobparameters - JSON, amend(JSON X, only primitive types)

--- batch/jobParameters/src/main/java/com/system/batch/config/SystemTerminatorConfig.java
+++ batch/jobParameters/src/main/java/com/system/batch/config/SystemTerminatorConfig.java
@@ -4,14 +4,20 @@ import com.system.batch.jobParameters.SystemInfiltrationParameters;
 import com.system.batch.util.QuestDifficulty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.converter.JobParametersConverter;
+import org.springframework.batch.core.converter.JsonJobParametersConverter;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.batch.JobLauncherApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -32,6 +38,11 @@ public class SystemTerminatorConfig {
                 .tasklet(terminatorTasklet, transactionManager)
                 .build();
     }
+
+//    @Bean
+//    public JobParametersConverter jobParametersConverter() {
+//        return new JsonJobParametersConverter();
+//    }
 
 //    @Bean
 //    @StepScope
@@ -80,29 +91,50 @@ public class SystemTerminatorConfig {
 //        };
 //    }
 
+//    @Bean
+//    @StepScope
+//    public Tasklet terminatorTasklet(
+//            SystemInfiltrationParameters systemInfiltrationParameters
+//    ) {
+//        return (contribution, chunkContext) -> {
+//            log.info("Processing terminator tasklet");
+//            log.info("systemInfiltrationParamters is injected : {}", systemInfiltrationParameters.getMissionName());
+//            log.info("systemInfiltrationParamters is injected : {}", systemInfiltrationParameters.getSecurityLevel());
+//            log.info("systemInfiltrationParamters is injected : {}", systemInfiltrationParameters.getOperationCommander());
+//
+//            log.info("reward is based operation Commander.");
+//            int reward = switch (systemInfiltrationParameters.getSecurityLevel()){
+//                case 1 -> 1;
+//                case 2 -> 2;
+//                case 3 -> 3;
+//                case 4 -> 4;
+//                default -> 1;
+//            };
+//            log.info("finially reward is : {}", reward);
+//
+//            log.info("Terminator tasklet finished");
+//            return RepeatStatus.FINISHED;
+//        };
+//    }
+
     @Bean
     @StepScope
     public Tasklet terminatorTasklet(
-            SystemInfiltrationParameters systemInfiltrationParameters
+            @Value("#{jobParameters['infiltrationTargets']}") String infiltrationTargets
     ) {
         return (contribution, chunkContext) -> {
-            log.info("Processing terminator tasklet");
-            log.info("systemInfiltrationParamters is injected : {}", systemInfiltrationParameters.getMissionName());
-            log.info("systemInfiltrationParamters is injected : {}", systemInfiltrationParameters.getSecurityLevel());
-            log.info("systemInfiltrationParamters is injected : {}", systemInfiltrationParameters.getOperationCommander());
 
-            log.info("reward is based operation Commander.");
-            int reward = switch (systemInfiltrationParameters.getSecurityLevel()){
-                case 1 -> 1;
-                case 2 -> 2;
-                case 3 -> 3;
-                case 4 -> 4;
-                default -> 1;
-            };
-            log.info("finially reward is : {}", reward);
+            //String[] targets = infiltrationTargets.split(",");
+            String target1 = infiltrationTargets;
+            //String target2 = targets[1];
+
+            log.info("Processing terminator tasklet");
+            log.info("systemInfiltrationParamters is approaching first target : {}", target1);
+            //log.info("systemInfiltrationParamters is approaching second target : {}", target2);
 
             log.info("Terminator tasklet finished");
             return RepeatStatus.FINISHED;
         };
     }
+
 }
