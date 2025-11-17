c4f6061e5b552ee83d34dcc5636a1c01cde3846a
leehyokyun <gyrbs22@gmail.com>
2025-11-17 13:08:29 +0900

    @Component - JobParameterValidator

--- batch/jobParameters/src/main/java/com/system/batch/jobParametersValidator/SystemDestructionValidator.java
+++ batch/jobParameters/src/main/java/com/system/batch/jobParametersValidator/SystemDestructionValidator.java
@@ -3,7 +3,9 @@ package com.system.batch.jobParametersValidator;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SystemDestructionValidator implements JobParametersValidator {
 
     @Override
