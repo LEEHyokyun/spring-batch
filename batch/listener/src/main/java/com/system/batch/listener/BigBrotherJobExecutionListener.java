449fff562d42c9b24f242bc958eb6f0ce3f645b0
leehyokyun <gyrbs22@gmail.com>
2025-11-19 15:55:09 +0900

    batch listener - using job/step listener annotation

--- batch/listener/src/main/java/com/system/batch/listener/BigBrotherJobExecutionListener.java
+++ batch/listener/src/main/java/com/system/batch/listener/BigBrotherJobExecutionListener.java
@@ -3,19 +3,31 @@ package com.system.batch.listener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.annotation.AfterJob;
+import org.springframework.batch.core.annotation.BeforeJob;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class BigBrotherJobExecutionListener implements JobExecutionListener {
-    @Override
-    public void beforeJob(JobExecution jobExecution){
-      log.info("This is BigBrotherJobExecutionListener.beforeJob");
-    }
+//    @Override
+//    public void beforeJob(JobExecution jobExecution){
+//      log.info("This is BigBrotherJobExecutionListener.beforeJob");
+//    }
 
-    @Override
-    public void afterJob(JobExecution jobExecution){
-        log.info("This is BigBrotherJobExecutionListener.afterJob");
-        log.info("Check the jobExecution status : {}", jobExecution.getStatus());
-    }
+      @BeforeJob
+      public void beforeJob(){
+        log.info("This is BigBrotherJobExecutionListener.beforeJob");
+      }
+
+//    @Override
+//    public void afterJob(JobExecution jobExecution){
+//        log.info("This is BigBrotherJobExecutionListener.afterJob");
+//        log.info("Check the jobExecution status : {}", jobExecution.getStatus());
+//    }
+
+      @AfterJob
+      public void afterJob(){
+          log.info("This is BigBrotherJobExecutionListener.afterJob");
+      }
 }
--- batch/listener/src/main/java/com/system/batch/listener/BigBrotherStepExecutionListener.java
+++ batch/listener/src/main/java/com/system/batch/listener/BigBrotherStepExecutionListener.java
@@ -4,20 +4,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class BigBrotherStepExecutionListener implements StepExecutionListener {
-    @Override
-    public void beforeStep(StepExecution stepExecution) {
-        log.info("This is BigBrotherStepExecutionListener.beforeStep");
-    }
+//    @Override
+//    public void beforeStep(StepExecution stepExecution) {
+//        log.info("This is BigBrotherStepExecutionListener.beforeStep");
+//    }
 
-    @Override
-    public ExitStatus afterStep(StepExecution stepExecution) {
-        log.info("This is BigBrotherStepExecutionListener.afterStep");
-        log.info("Check the stepExecution status : {}", stepExecution.getStatus());
-        return ExitStatus.COMPLETED;
-    }
+      @BeforeStep
+      public void beforeStep(StepExecution stepExecution){
+          log.info("This is BigBrotherStepExecutionListener.beforeStep");
+      }
+
+
+//    @Override
+//    public ExitStatus afterStep(StepExecution stepExecution) {
+//        log.info("This is BigBrotherStepExecutionListener.afterStep");
+//        log.info("Check the stepExecution status : {}", stepExecution.getStatus());
+//        return ExitStatus.COMPLETED;
+//    }
+
+      @AfterStep
+      public ExitStatus afterStep(StepExecution stepExecution){
+          log.info("This is BigBrotherStepExecutionListener.afterStep");
+          return ExitStatus.COMPLETED;
+      }
 }
