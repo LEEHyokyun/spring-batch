73e91b5c4f9b96b49df39c99ddb06e0572c62155
leehyokyun <gyrbs22@gmail.com>
2025-12-12 19:01:44 +0900

    spring batch postgresql

--- batch/postgresql/src/main/java/com/system/batch/config/CustomizedFactoryTerminatedVictimConfig.java
+++ batch/postgresql/src/main/java/com/system/batch/config/CustomizedFactoryTerminatedVictimConfig.java
@@ -1,97 +1,97 @@
-//package com.system.batch.config;
-//
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.batch.core.Job;
-//import org.springframework.batch.core.Step;
-//import org.springframework.batch.core.job.builder.JobBuilder;
-//import org.springframework.batch.core.repository.JobRepository;
-//import org.springframework.batch.core.step.builder.StepBuilder;
-//import org.springframework.batch.item.ItemWriter;
-//import org.springframework.batch.item.database.JdbcPagingItemReader;
-//import org.springframework.batch.item.database.Order;
-//import org.springframework.batch.item.database.PagingQueryProvider;
-//import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
-//import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.jdbc.core.BeanPropertyRowMapper;
-//import org.springframework.transaction.PlatformTransactionManager;
-//
-//import javax.sql.DataSource;
-//import java.time.LocalDateTime;
-//import java.util.Map;
-//
-//@Slf4j
-//@Configuration
-//@RequiredArgsConstructor
-//public class CustomizedFactoryTerminatedVictimConfig {
-//    private final JobRepository jobRepository;
-//    private final PlatformTransactionManager transactionManager;
-//    private final DataSource dataSource;
-//
-//    @Bean
-//    public Job processVictimJob() throws Exception {
-//        return new JobBuilder("victimRecordJob", jobRepository)
-//                .start(processVictimStep())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step processVictimStep() throws Exception {
-//        return new StepBuilder("victimRecordStep", jobRepository)
-//                .<Victim, Victim>chunk(5, transactionManager)
-//                .reader(terminatedVictimReader(dataSource))
-//                .writer(victimWriter())
-//                .build();
-//    }
-//
-//    @Bean
-//    public JdbcPagingItemReader<Victim> terminatedVictimReader(DataSource dataSource) throws Exception {
-//        return new JdbcPagingItemReaderBuilder<Victim>()
-//                .name("terminatedVictimReader")
-//                .dataSource(dataSource)
-//                .queryProvider(pagingQueryProvider(dataSource)) // 커스텀 PagingQueryProvider 적용
-//                .parameterValues(Map.of(
-//                        "status", "TERMINATED",
-//                        "terminatedAt", LocalDateTime.now().minusDays(1))
-//                )
-//                .pageSize(5)
-//                .rowMapper(new BeanPropertyRowMapper<>(Victim.class))
-//                .build();
-//    }
-//
-//    private PagingQueryProvider pagingQueryProvider(DataSource dataSource) throws Exception {
-//        SqlPagingQueryProviderFactoryBean queryProviderFactory = new SqlPagingQueryProviderFactoryBean();
-//
-//        // 데이터베이스 타입에 맞는 적절한 PagingQueryProvider 구현체를 생성할 수 있도록 dataSource를 전달해줘야 한다.
-//        queryProviderFactory.setDataSource(dataSource);
-//        queryProviderFactory.setSelectClause("SELECT id, name, process_id, terminated_at, status");
-//        queryProviderFactory.setFromClause("FROM victims");
-//        queryProviderFactory.setWhereClause("WHERE status = :status AND terminated_at <= :terminatedAt");
-//        queryProviderFactory.setSortKeys(Map.of("id", Order.ASCENDING));
-//
-//        return queryProviderFactory.getObject();
-//    }
-//
-//    @Bean
-//    public ItemWriter<Victim> victimWriter() {
-//        return items -> {
-//            for (Victim victim : items) {
-//                log.info("{}", victim);
-//            }
-//        };
-//    }
-//
-//    @NoArgsConstructor
-//    @Data
-//    public static class Victim {
-//        private Long id;
-//        private String name;
-//        private String processId;
-//        private LocalDateTime terminatedAt;
-//        private String status;
-//    }
-//}
+package com.system.batch.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class CustomizedFactoryTerminatedVictimConfig {
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final DataSource dataSource;
+
+    @Bean
+    public Job processVictimJob() throws Exception {
+        return new JobBuilder("victimRecordJob", jobRepository)
+                .start(processVictimStep())
+                .build();
+    }
+
+    @Bean
+    public Step processVictimStep() throws Exception {
+        return new StepBuilder("victimRecordStep", jobRepository)
+                .<Victim, Victim>chunk(5, transactionManager)
+                .reader(terminatedVictimReader(dataSource))
+                .writer(victimWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Victim> terminatedVictimReader(DataSource dataSource) throws Exception {
+        return new JdbcPagingItemReaderBuilder<Victim>()
+                .name("terminatedVictimReader")
+                .dataSource(dataSource)
+                .queryProvider(pagingQueryProvider(dataSource)) // 커스텀 PagingQueryProvider 적용
+                .parameterValues(Map.of(
+                        "status", "TERMINATED",
+                        "terminatedAt", LocalDateTime.now().minusDays(1))
+                )
+                .pageSize(5)
+                .rowMapper(new BeanPropertyRowMapper<>(Victim.class))
+                .build();
+    }
+
+    private PagingQueryProvider pagingQueryProvider(DataSource dataSource) throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProviderFactory = new SqlPagingQueryProviderFactoryBean();
+
+        // 데이터베이스 타입에 맞는 적절한 PagingQueryProvider 구현체를 생성할 수 있도록 dataSource를 전달해줘야 한다.
+        queryProviderFactory.setDataSource(dataSource);
+        queryProviderFactory.setSelectClause("SELECT id, name, process_id, terminated_at, status");
+        queryProviderFactory.setFromClause("FROM victims");
+        queryProviderFactory.setWhereClause("WHERE status = :status AND terminated_at <= :terminatedAt");
+        queryProviderFactory.setSortKeys(Map.of("id", Order.ASCENDING));
+
+        return queryProviderFactory.getObject();
+    }
+
+    @Bean
+    public ItemWriter<Victim> victimWriter() {
+        return items -> {
+            for (Victim victim : items) {
+                log.info("{}", victim);
+            }
+        };
+    }
+
+    @NoArgsConstructor
+    @Data
+    public static class Victim {
+        private Long id;
+        private String name;
+        private String processId;
+        private LocalDateTime terminatedAt;
+        private String status;
+    }
+}
