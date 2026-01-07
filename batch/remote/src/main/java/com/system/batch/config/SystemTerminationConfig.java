83751278c11893fa40472333978e95486b3d7e26
leehyokyun <gyrbs22@gmail.com>
2026-01-07 20:00:50 +0900

    when extends DefaultBatchConfiguraiton - manually configured needed

--- batch/remote/src/main/java/com/system/batch/config/SystemTerminationConfig.java
+++ batch/remote/src/main/java/com/system/batch/config/SystemTerminationConfig.java
@@ -2,84 +2,71 @@ package com.system.batch.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.ExecutionContextSerializer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.dao.Jackson2ExecutionContextStringSerializer;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.batch.BatchDataSource;
+import org.springframework.boot.autoconfigure.batch.BatchDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.boot.autoconfigure.batch.JobLauncherApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.sql.init.OnDatabaseInitializationCondition;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.util.StringUtils;
 
+import javax.sql.DataSource;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
-public class SystemTerminationConfig {
-    private AtomicInteger processesKilled = new AtomicInteger(0);
-    private final int TERMINATION_TARGET = 5;
-
-    private final JobRepository jobRepository;
-    private final PlatformTransactionManager transactionManager;
-
-    public SystemTerminationConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        this.jobRepository = jobRepository;
-        this.transactionManager = transactionManager;
+@EnableConfigurationProperties(BatchProperties.class)
+public class SystemTerminationConfig extends DefaultBatchConfiguration {
+    @Override
+    protected ExecutionContextSerializer getExecutionContextSerializer() {
+        return new Jackson2ExecutionContextStringSerializer();
     }
 
     @Bean
-    public Job systemTerminationSimulationJob(){
-        return new JobBuilder("systemTerminationSimulationJob", jobRepository)
-                .start(enterWorldStep())
-                .next(meetNPCStep())
-                .next(defeatProcessStep())
-                .next(completeQuestStep())
-                .build();
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "spring.batch.job", name = "enabled", havingValue = "true", matchIfMissing = true)
+    public JobLauncherApplicationRunner jobLauncherApplicationRunner(JobLauncher jobLauncher, JobExplorer jobExplorer,
+                                                                     JobRepository jobRepository, BatchProperties properties) {
+        JobLauncherApplicationRunner runner = new JobLauncherApplicationRunner(jobLauncher, jobExplorer, jobRepository);
+        String jobName = properties.getJob().getName();
+        if (StringUtils
+                .hasText(jobName)) {
+            runner.setJobName(jobName);
+        }
+        return runner;
     }
 
-    @Bean
-    public Step enterWorldStep(){
-        return new StepBuilder("enterWorldStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Entered to System Termination World.");
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
+    @Configuration(proxyBeanMethods = false)
+    @Conditional(OnBatchDatasourceInitializationCondition.class)
+    static class DataSourceInitializerConfiguration {
+
+        @Bean
+        @ConditionalOnMissingBean
+        BatchDataSourceScriptDatabaseInitializer batchDataSourceInitializer(DataSource dataSource,
+                                                                            @BatchDataSource ObjectProvider<DataSource> batchDataSource, BatchProperties properties) {
+            return new BatchDataSourceScriptDatabaseInitializer(batchDataSource.getIfAvailable(() -> dataSource),
+                    properties.getJdbc());
+        }
     }
 
-    @Bean
-    public Step meetNPCStep(){
-        return new StepBuilder("meetNPCStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("meet NPC.");
-                    System.out.println("First mission : Zombie process : " + TERMINATION_TARGET + " has been Killed");
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    @Bean
-    public Step defeatProcessStep(){
-        return new StepBuilder("defeatProcessStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    int terminated = processesKilled.incrementAndGet();
-                    System.out.println("defeatProcessStep is Running : target has been killed by "+ terminated);
-
-                    if(terminated < TERMINATION_TARGET){
-                        return RepeatStatus.CONTINUABLE;
-                    }else{
-                        return RepeatStatus.FINISHED;
-                    }
-                }, transactionManager)
-                .build();
-    }
-
-    @Bean
-    public Step completeQuestStep(){
-        return new StepBuilder("completeQuestStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("complete quest.");
-                    System.out.println("Congratulations! Your Batch Step has just reached the basic one!");
-                    return  RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
+    static class OnBatchDatasourceInitializationCondition extends OnDatabaseInitializationCondition {
+        OnBatchDatasourceInitializationCondition() {
+            super("Batch", "spring.batch.jdbc.initialize-schema", "spring.batch.initialize-schema");
+        }
     }
 }
