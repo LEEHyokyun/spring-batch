66623f0464f025491673d026030a0fdc5dccca7e
leehyokyun <gyrbs22@gmail.com>
2026-01-13 17:28:29 +0900

    multiThread - SynchronizedItemStreamReader for thread safe(*basic structure)

--- batch/flow/src/main/java/com/system/batch/flow/LectureValidationFlow.java
+++ batch/flow/src/main/java/com/system/batch/flow/LectureValidationFlow.java
@@ -1,25 +1,25 @@
-package com.system.batch.flow;
-
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
-
-@Component
-public class LectureValidationFlow {
-    @Bean
-    public Flow lectureValidationFlow() {
-        return new FlowBuilder<Flow>("lectureValidationFlow")
-                .start(validateContentStep)
-                .next(checkPlagiarismStep)
-                .on("PLAGIARISM_DETECTED").fail()  // 표절 감지되면 즉시 실패 처리
-                .from(checkPlagiarismStep)
-                .on("COMPLETED").to(verifyPricingStep)
-                .on("TOO_EXPENSIVE").to(pricingWarningStep)  // 가격이 과도하면 경고 처리
-                .from(verifyPricingStep)
-                .on("*").end()  // 나머지 모든 경우는 정상 종료
-                .build();
-    }
-}
+//package com.system.batch.flow;
+//
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.job.builder.FlowBuilder;
+//import org.springframework.batch.core.job.flow.Flow;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//
+//@Component
+//public class LectureValidationFlow {
+//    @Bean
+//    public Flow lectureValidationFlow() {
+//        return new FlowBuilder<Flow>("lectureValidationFlow")
+//                .start(validateContentStep)
+//                .next(checkPlagiarismStep)
+//                .on("PLAGIARISM_DETECTED").fail()  // 표절 감지되면 즉시 실패 처리
+//                .from(checkPlagiarismStep)
+//                .on("COMPLETED").to(verifyPricingStep)
+//                .on("TOO_EXPENSIVE").to(pricingWarningStep)  // 가격이 과도하면 경고 처리
+//                .from(verifyPricingStep)
+//                .on("*").end()  // 나머지 모든 경우는 정상 종료
+//                .build();
+//    }
+//}
